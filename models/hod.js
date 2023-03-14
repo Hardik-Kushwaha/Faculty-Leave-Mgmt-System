e3c53ee6f4865b2a171cd789108d37d9f2fef3ff
Harshit_Dongre <95414266+Harshit1020@users.noreply.github.com>
2023-03-15 00:35:22 +0530

    done

--- models/hod.js
+++ models/hod.js
@@ -5,7 +5,11 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var hodSchema = new mongoose.Schema({
   name: String,
   type: String,
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true 
+  },
   password: String,
   department: String,
   image: {
